a8b983528f4f9ee950ec56b54b60933beca038bc
SpektralDevelopment <spektraldevelopment@gmail.com>
2013-12-02 09:57:24 -0500

    Decided to not pull in data from LinkedIn for now.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 //////////////////
-////LINKEDIN JS - Coming soon
-///////////////////
-//Pulls my public profile from linkedin to populate certain parts of my resume
-
-//////////////////
 ////INFO ANCHOR TAGS
 //////////////////
 $('#info a').click(aTagClicked);
